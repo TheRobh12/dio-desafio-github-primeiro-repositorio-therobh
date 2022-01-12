1cf0d9487d63e20bc7fd4bfe7a7751f41bcd7ba5
Robson <robsonbastossantos@gmail.com>
2022-01-12 13:35:33 -0300

    Exercises Cognizant part 2

---
+++ CognizantExerciciosPart2/src/one/digitalinnovation/basecamp/Mensagem.java
@@ -0,0 +1,32 @@
+package one.digitalinnovation.basecamp;
+
+    /*
+     * Classe de Exemplo para o Exercicioda aula 1 de Métodos
+     */
+
+public class Mensagem {
+
+    public static void obterMensagem(int hora) {
+
+        switch (hora) {
+            case 5, 6, 7, 8, 9, 10, 11, 12 -> mensagemBomDia();
+            case 13, 14, 15, 16, 17 -> mensagemBoaTarde();
+            case 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4 -> mensagemBoaNoite();
+            default -> System.out.println("Hora Inválida!");
+        }
+
+    }
+
+    private static void mensagemBomDia() {
+        System.out.println("Bom Dia!");
+    }
+
+    private static void mensagemBoaTarde() {
+        System.out.println("Boa Tarde!");
+    }
+
+    private static void mensagemBoaNoite() {
+        System.out.println("Boa Noite!");
+    }
+
+}
